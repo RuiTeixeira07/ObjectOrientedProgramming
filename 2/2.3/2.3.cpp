6c5fb1e7a629d7d5ee6c0efe7c9daf228f0dd570
Rui Teixeira <al2025155352@alunos.utad.pt>
2026-04-19 19:28:28 +0100

    Add '2.3'.

---
+++ 2/2.3/2.3.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define MAX 20
+#define OUTPUT_DATA_FILE_PATH "Assets/Students.txt"
+#define FILE_SEPARATOR ";"
+
+typedef struct Student
+{
+    char number[MAX];
+    std::string name;
+    int age;
+} STUDENT;
+
+void main()
+{
+    std::ofstream file;
+    int numberOfStudents;
+
+    std::cout << "How many students do you want to register?" << std::endl;
+    std::cin >> numberOfStudents;
+
+    file.open(OUTPUT_DATA_FILE_PATH);
+
+	if (file.is_open() == false)
+	{
+		std::cout << "Error: Could Not Open File." << std::endl;
+		return;
+	}
+
+	for (int i = 0; i < numberOfStudents; i++)
+	{
+		STUDENT student;
+
+		std::cout << "Insert your student number: " << std::endl;
+		std::cin >> student.number;
+
+		std::cout << "Insert your name: " << std::endl;
+		std::getline(std::cin >> std::ws, student.name);
+
+		std::cout << "Insert your age: " << std::endl;
+		std::cin >> student.age;
+
+		file << student.number << FILE_SEPARATOR << student.name << FILE_SEPARATOR << student.age << std::endl;
+	}
+
+    file.close();
+}
