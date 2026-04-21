e297b24bac0648f0d0c544a1fb896b5a118e2879
Rui Teixeira <al2025155352@alunos.utad.pt>
2026-04-21 18:10:09 +0100

    Add '3.5'.

---
+++ 3/3.5/Date.cpp
@@ -0,0 +1,9 @@
+
+#include "Date.h"
+
+void Date::update(int day, int month, int year)
+{
+    _day = day;
+    _month = month;
+    _year = year;
+}
---
+++ 3/3.5/Date.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+class Date
+{
+	private:
+		int _day;
+		int _month;
+		int _year;
+
+	public:
+		void update(int day, int month, int year);
+};
